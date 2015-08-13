b10dc5b3fde8770a9b223c12c148f70afcd46aef
Thomas Elliott <tom.w.elliott@gmail.com>
2015-08-12 20:50:53 -0400

    Add test for a storyboard with wraparound.

--- TEQuickPageViewControllerTests/TEQuickPageViewControllerTests.m
+++ TEQuickPageViewControllerTests/TEQuickPageViewControllerTests.m
@@ -27,12 +27,58 @@
 }
 
 /**
+ * Test a 4 page layout with wraparound
+ */
+- (void)testItBuildsA4PageLayoutWithWraparound {
+    
+    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Test4PageWithWraparound" bundle:bundle];
+    
+    // Get the initial view controller for the example storyboard, should set up the board
+    TEQuickPageViewController *vc = [sb instantiateInitialViewController];
+    [vc viewDidLoad];
+    
+    int TOTAL_PAGES = 4;
+    
+    // Check that we have 4 pages
+    XCTAssert(TOTAL_PAGES == [vc presentationCountForPageViewController:vc], @"Expected 4 pages.");
+    
+    // Pull the first page
+    UIViewController* currentPage = [vc viewControllerAtIndex:0];
+    XCTAssert(currentPage != nil, @"Expected a first page!");
+
+    // Make three forward passes over the page sequence
+    
+    for(int i = 0; i < TOTAL_PAGES * 3; i++){
+        int position = i%TOTAL_PAGES;
+       NSString* expected = [NSString stringWithFormat:@"View%d",position];
+        XCTAssert([currentPage.restorationIdentifier isEqualToString:expected],@"Received unexpected identifier for view %d: %@, expected %@", i, currentPage.restorationIdentifier, expected);
+        currentPage = [vc pageViewController:vc viewControllerAfterViewController:currentPage];
+    }
+    
+    // Make three reverse passes over the page sequence
+    
+    currentPage = [vc viewControllerAtIndex:0];
+    
+    for(int i = 0; i < TOTAL_PAGES * 3; i++){
+        int position = TOTAL_PAGES - (i%TOTAL_PAGES);
+        if(position == TOTAL_PAGES){
+            position = 0;
+        }
+        NSString* expected = [NSString stringWithFormat:@"View%d",position];
+        XCTAssert([currentPage.restorationIdentifier isEqualToString:expected],@"Received unexpected identifier for view %d: %@, expected %@", i, currentPage.restorationIdentifier, expected);
+        currentPage = [vc pageViewController:vc viewControllerBeforeViewController:currentPage];
+    }
+    
+}
+
+/**
  * Test a 4 page layout with no wraparound
  */
 - (void)testItBuildsA4PageLayoutNoWraparound {
     
     NSBundle *bundle = [NSBundle bundleForClass:[self class]];
-    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Test4Page" bundle:bundle];
+    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Test4PageNoWraparound" bundle:bundle];
     
     // Get the initial view controller for the example storyboard, should set up the board
     TEQuickPageViewController *vc = [sb instantiateInitialViewController];
